d149ec5eb74a036621701a1f07fe895d1799e2c5
xscr33m <xscr33m@gmail.com>
2023-12-09 11:35:33 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "xscr33m's BigFile Checker for GitHub-Repos v0.1";
+
+        string coloredTextStart = $"================================================= \n" +
+                                  $"||                                            || \n" +
+                                  $"|| xscr33m's BigFile Checker for GitHub-Repos || \n" +
+                                  $"||                                            || \n" +
+                                  $"=================================================";
+        string coloredTextExit = $"< Press any key to exit the program >";
+
+        WriteColoredConsoleText(coloredTextStart);
+
+        WriteColoredConsoleText($"{DateTime.Now}: [INFO] < Initializing.... > \n", ConsoleColor.White);
+
+        if (args.Length == 0)
+        {
+            WriteColoredConsoleText($"{DateTime.Now}: [ERROR] Please drag the folder path to the executable file. \n", ConsoleColor.Red);
+
+            WriteColoredConsoleText(coloredTextExit);
+            Console.ReadKey();
+            return;
+        }
+
+        string folderPath = args[0];
+
+        if (!Directory.Exists(folderPath))
+        {
+            WriteColoredConsoleText($"{DateTime.Now}: [ERROR] The specified folder path does not exist. \n", ConsoleColor.Red);
+
+            WriteColoredConsoleText(coloredTextExit);
+            Console.ReadKey();
+            return;
+        }
+
+        string gitIgnoreFilePath = Path.Combine(folderPath, ".gitignore");
+        string logFilePath = Path.Combine(folderPath, "xscr33ms_BigFile_Checker_Log.txt");
+
+        WriteColoredConsoleText($"{DateTime.Now}: [INFO] Scanning files larger than 100 MB in the following folder: \n{folderPath}.... \n", ConsoleColor.White);
+
+        List<string> largeFiles = GetLargeFiles(folderPath, 100 * 1024 * 1024);
+        List<string> mediumFiles = GetLargeFiles(folderPath, 50 * 1024 * 1024);
+
+        if (largeFiles.Count == 0 || mediumFiles.Count == 0)
+        {
+            WriteColoredConsoleText($"################################################", ConsoleColor.Red);
+            WriteColoredConsoleText($"Files found larger than 100 MB → COUNT:", ConsoleColor.White);
+            WriteColoredConsoleText($" == {largeFiles.Count} == \n", ConsoleColor.Red);
+            WriteColoredConsoleText($"Files found larger than 50 MB → COUNT:", ConsoleColor.White);
+            WriteColoredConsoleText($" == {mediumFiles.Count} == \n", ConsoleColor.Red);
+
+            WriteColoredConsoleText($"[INFO] No files bigger than 100 or 50 MB were found! \n" +
+                                    $"No .gitignore were touched or created! \n" +
+                                    $"If you want to ignore files from your repo, create a .gitignore manually!", ConsoleColor.Green);
+            WriteColoredConsoleText($"################################################ \n", ConsoleColor.Red);
+
+            WriteColoredConsoleText(coloredTextExit);
+            Console.ReadKey();
+            return;
+        }
+        else
+        {
+            WriteColoredConsoleText($"################################################", ConsoleColor.Red);
+            WriteColoredConsoleText($"Files found larger than 100 MB → COUNT:", ConsoleColor.White);
+            WriteColoredConsoleText($" == {largeFiles.Count} == \n", ConsoleColor.Red);
+            WriteColoredConsoleText($"Files found larger than 50 MB → COUNT:", ConsoleColor.White);
+            WriteColoredConsoleText($" == {mediumFiles.Count} == \n", ConsoleColor.Red);
+
+            WriteColoredConsoleText($"[IMPORTANT] Found files gonna be added to your existing .gitignore, regardless if already existing. \n" +
+                                    $"I highly suggest to only run the code once or update your template-gitignore with it. \n" +
+                                    $"Make sure to validate your .gitignore file created here:", ConsoleColor.DarkYellow);
+            WriteColoredConsoleText($"{gitIgnoreFilePath} \n", ConsoleColor.White);
+            WriteColoredConsoleText($"[INFO] You find all files >50 MB that have been found in the logfile created here:", ConsoleColor.Green);
+            WriteColoredConsoleText($"{logFilePath}", ConsoleColor.White);
+            WriteColoredConsoleText($"################################################ \n", ConsoleColor.Red);
+
+            UpdateGitIgnoreFile(gitIgnoreFilePath, largeFiles);
+            CreateLogFile(logFilePath, largeFiles, mediumFiles);
+
+            WriteColoredConsoleText($"[INFO] < Finishing.... > \n", ConsoleColor.White);
+
+            WriteColoredConsoleText(coloredTextExit);
+            Console.ReadKey();
+        }
+
+    }
+
+    static List<string> GetLargeFiles(string folderPath, long sizeLimit)
+    {
+        DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+
+        return directoryInfo.GetFiles("*.*", SearchOption.AllDirectories)
+            .Where(file => file.Length > sizeLimit)
+            .Select(file => file.FullName)
+            .ToList();
+    }
+
+    static void UpdateGitIgnoreFile(string gitIgnoreFilePath, List<string> files)
+    {
+        WriteColoredConsoleText($"{DateTime.Now}: [INFO] Updating .gitignore file....", ConsoleColor.White);
+
+        StringBuilder gitIgnoreContent = new StringBuilder();
+
+        if (File.Exists(gitIgnoreFilePath))
+        {
+            gitIgnoreContent.Append(File.ReadAllText(gitIgnoreFilePath));
+            gitIgnoreContent.AppendLine();
+        }
+
+        gitIgnoreContent.AppendLine("# xscr33m's BigFile Checker for GitHub-Repos");
+        gitIgnoreContent.AppendLine("# Found files bigger than 100 MB:");
+
+        foreach (string file in files)
+        {
+            string relativePath = GetRelativePath(gitIgnoreFilePath, file);
+            gitIgnoreContent.AppendLine(relativePath);
+        }
+
+        gitIgnoreContent.AppendLine($"{Environment.NewLine}# xscr33m's BigFile Checker Log-File:");
+        gitIgnoreContent.AppendLine("xscr33ms_BigFile_Checker_Log.txt");
+
+        // Save the file in UTF-8 format with BOM
+        File.WriteAllText(gitIgnoreFilePath, gitIgnoreContent.ToString(), Encoding.UTF8);
+
+        WriteColoredConsoleText($"{DateTime.Now}: [INFO] .gitignore file successfully updated. \n", ConsoleColor.Green);
+    }
+
+    /*************************************
+     *    .gitignore examples:           *
+     * FOLDER_Name  =  specific folder   *
+     * File.txt     =  specific filename *
+     ************************************/
+    static string GetRelativePath(string basePath, string fullPath)
+    {
+        Uri baseUri = new Uri(basePath);
+        Uri fullUri = new Uri(fullPath);
+
+        return Uri.UnescapeDataString("/" + baseUri.MakeRelativeUri(fullUri).ToString().Replace('\\', Path.DirectorySeparatorChar));
+    }
+
+    static void CreateLogFile(string logFilePath, List<string> files, List<string> files1)
+    {
+        WriteColoredConsoleText($"{DateTime.Now}: [INFO] Creating log file...", ConsoleColor.White);
+
+        using (StreamWriter writer = new StreamWriter(logFilePath, true))
+        {
+            if (logFilePath != null)
+            {
+                writer.WriteLine("================================================");
+                writer.WriteLine("||                                            ||");
+                writer.WriteLine("|| xscr33m's BigFile Checker for GitHub-Repos ||");
+                writer.WriteLine("||                                            ||");
+                writer.WriteLine("================================================");
+
+                writer.WriteLine($"{DateTime.Now}: [INFO] Found files >100 MB:", ConsoleColor.White);
+                foreach (string file in files)
+                {
+                    writer.WriteLine($"{file} - {GetSizeString(new FileInfo(file).Length)}", ConsoleColor.White);
+                }
+
+                writer.WriteLine($"\n{DateTime.Now}: [INFO] Found files >50 MB:", ConsoleColor.White);
+                foreach (string file in files1)
+                {
+                    writer.WriteLine($"{file} - {GetSizeString(new FileInfo(file).Length)}", ConsoleColor.White);
+                }
+
+                writer.WriteLine();
+            }
+        }
+
+        WriteColoredConsoleText($"{DateTime.Now}: [INFO] Log file successfully created. \n", ConsoleColor.Green);
+    }
+
+    static string GetSizeString(long sizeInBytes)
+    {
+        string[] sizes = { "B", "KB", "MB", "GB", "TB" };
+        int order = 0;
+        double size = sizeInBytes;
+
+        while (size >= 1024 && order < sizes.Length - 1)
+        {
+            order++;
+            size /= 1024;
+        }
+
+        return $"{size:0.##} {sizes[order]}";
+    }
+
+    private static void WriteColoredConsoleText(string coloredText, ConsoleColor? color = null)
+    {
+        if (color.HasValue)
+        {
+            Console.ForegroundColor = color.Value;
+            foreach (char character in coloredText)
+            {
+                Console.Write(character);
+            }
+
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+        else
+        {
+            Random random = new Random();
+            for (int i = 0; i < coloredText.Length; i++)
+            {
+                ConsoleColor randomColor = (ConsoleColor)random.Next(3, 7);
+
+                Console.ForegroundColor = randomColor;
+
+                Console.Write(coloredText[i]);
+
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+﻿[![Pricing](https://img.shields.io/badge/Price-Free-green?style=for-the-badge&color=green)](https://github.com/xscr33m/BigFile_Checker/)
+[![Downloads](https://img.shields.io/github/downloads/xscr33m/BigFile_Checker/total?style=for-the-badge&color=gold)](https://github.com/xscr33m/BigFile_Checker/releases)
+[![GitHub last commit (branch)](https://img.shields.io/github/last-commit/xscr33m/BigFile_Checker/Master?style=for-the-badge&color=gold)](https://github.com/xscr33m/BigFile_Checker/commits/Master/)
+[![GitHub License](https://img.shields.io/github/license/xscr33m/BigFile_Checker?style=for-the-badge&color=gold)](https://spdx.org/licenses/)
+[![Discord](https://img.shields.io/discord/1102440447835648124?style=for-the-badge&label=Discord&color=gold)](https://discord.com/invite/PasvscT4Nh)
+
+ 
+## xscr33m's BigFile Checker for GitHub-Repo-Folder
+
+Check Big Repos for Files bigger than the allowed 100 MB and create or update a .gitignore. Nice and very fast Console and Log Output!
+
+
+## How-To-Use
+
+   1. Download the latest release of [xscr33m.s_BigFile_Checker.zip](https://github.com/xscr33m/BigFile_Checker/releases)  →→→→→→→→→→→→→→→→→
+   2. Unzip the "xscr33m.s_BigFile_Checker.zip" file with 7Zip or another program.
+   3. Open the extracted folder.
+   4. Drag your Repo-Folder you want to check on the "xscr33m.s_BigFile_Checker.exe" and watch the Magic happen!
+
+
+## Sneak-Preview
+
+![Logo](https://cdn.discordapp.com/attachments/1183147757612040324/1183166943256133762/BigFile_Checker_Preview.png)
+
+
+## License
+
+[MIT License](https://spdx.org/licenses/)
+
+
+## Feedback
+
+If you have any feedback, please reach out to me!
+I would appreciate it very much! 
+
+[![Logo](https://cdn.discordapp.com/attachments/1182770512133361754/1183151523581153462/Discord_Banner.png)](https://discord.com/invite/PasvscT4Nh)
+
+[![Logo](https://cdn.discordapp.com/attachments/1182770512133361754/1183155022419197982/PayPal_Donate.png)](https://www.paypal.com/paypalme/dheil53)
+
+# Made with ♥ for this amazing community!
